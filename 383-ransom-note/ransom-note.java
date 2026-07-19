0331543918d613768721a4ed87313c00c6fd6afa
Thushar shetty <shettythushar31@gmail.com>
2026-07-19 10:42:40 +0530

    Time: 11 ms (40.33%) | Memory: 47 MB (34.72%) - LeetSync

--- 383-ransom-note/ransom-note.java
+++ 383-ransom-note/ransom-note.java
@@ -1,20 +1,35 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-       HashMap<Character,Integer> map=new HashMap<>();
-       for(char ch:ransomNote.toCharArray()){
-        map.put(ch,map.getOrDefault(ch,0)+1);
-       }
-       for(char ch:magazine.toCharArray()){
-        if (map.containsKey(ch)) { 
-    map.put(ch, map.get(ch) - 1); 
-    if (map.get(ch) == 0) { 
-        map.remove(ch); 
-    }
+//        HashMap<Character,Integer> map=new HashMap<>();
+//        for(char ch:ransomNote.toCharArray()){
+//         map.put(ch,map.getOrDefault(ch,0)+1);
+//        }
+//        for(char ch:magazine.toCharArray()){
+//         if (map.containsKey(ch)) { 
+//     map.put(ch, map.get(ch) - 1); 
+//     if (map.get(ch) == 0) { 
+//         map.remove(ch); 
+//     }
+// }
+//        }
+//        if(map.size() >0){
+//      return   false;
+//        }
+//     return true;
+
+
+if(ransomNote.length() >magazine.length()) return false;
+ HashMap<Character,Integer> map=new HashMap<>();
+
+for(char ch:magazine.toCharArray()){
+    map.put(ch,map.getOrDefault(ch,0)+1);
 }
-       }
-       if(map.size() >0){
-     return   false;
-       }
-    return true;
+for(char ch:ransomNote.toCharArray()){
+    if(!map.containsKey(ch) || map.get(ch) ==0){
+        return false;
+    }
+    map.put(ch,map.get(ch) - 1);
+}
+return true;
     }
 }
