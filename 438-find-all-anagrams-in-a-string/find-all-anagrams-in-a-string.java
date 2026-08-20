e649c40d7b01c5bbf128f09e7a9c47c14011c6e0
Thushar shetty <shettythushar31@gmail.com>
2026-08-20 20:11:38 +0530

    Time: 11 ms (66.93%) | Memory: 47.1 MB (36.51%) - LeetSync

---
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+        List<Integer> ans=new ArrayList<>();
+       if(s.length()<p.length()){
+        return ans;
+       }
+       int pfeq[]=new int[26];
+       int window[]=new int[26];
+
+       for(char ch:p.toCharArray()){
+        pfeq[ch-'a']++;
+       }
+        int k = p.length();
+        for(int i=0;i<k;i++){
+            window[s.charAt(i)-'a']++;
+        }
+        if(Arrays.equals(pfeq,window)){
+            ans.add(0);
+        }
+        for(int i=k;i<s.length();i++){
+            window[s.charAt(i)-'a']++;
+            window[s.charAt(i-k)-'a']--;
+            if(Arrays.equals(pfeq,window)){
+                ans.add((i-k)+1);
+            }
+        }
+        return ans;
+    }
+}
