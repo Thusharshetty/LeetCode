54d698d20eecd9ffc0c66386454ee97fe8bf9a06
Thushar shetty <shettythushar31@gmail.com>
2026-09-14 21:06:28 +0530

    Time: 6 ms (29.15%) | Memory: 44.6 MB (82.91%) - LeetSync

--- 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.java
+++ 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.java
@@ -3,16 +3,14 @@ class Solution {
         Stack<Character> st=new Stack<>();
         int MinLength=s.length();
         for(int i=0;i<s.length();i++){
-            if(!st.isEmpty()&& st.peek()=='A' && s.charAt(i)=='B'){
-                st.pop();
-                MinLength=MinLength-2;
-            }else if(!st.isEmpty()&& st.peek()=='C' && s.charAt(i)=='D'){
+            char c=s.charAt(i);
+            if(!st.isEmpty()&& ((st.peek()=='A' && c=='B')||(st.peek()=='C' && c=='D'))){
                 st.pop();
                 MinLength=MinLength-2;
             }else{
                 st.push(s.charAt(i));
             }
         }
-        return MinLength;
+        return st.size();
     }
 }
